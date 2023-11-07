b956fcf3e47b94b644515a5ea20ec81f78b36a96
TopTen1004 <97684719+TopTen1004@users.noreply.github.com>
2023-11-07 06:01:46 +0100

    added new activity for login

---
+++ Android/app/src/main/java/com/example/uhf_bt/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.uhf_bt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+    }
+}
